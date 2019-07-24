7dac5ead179de2fd99cd87cc84190e2fc0d27a86
asfawayalk <asfaw.ayalkibet@gmail.com>
2019-07-24 11:22:06 +0300

    updating background

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import React, {Component} from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
+import Particles from 'react-particles-js';
 // import Logo from './components/Logo/Logo';
 
+const particleOptions ={
+                    particles: {
+                     number:{
+
+                        value:80,
+                        density:{
+                            enable:true,
+                            value_area:800
+                        }
+                     }
+                    }
+                }
 class App extends Component {
+
+
     constructor() {
         super()
         this.state = {
@@ -15,6 +30,11 @@ class App extends Component {
     render() {
         return(
             <div className="App">
+
+            <Particles className="particles" 
+              params={particleOptions}
+             
+            />
                 <Navigation />
                 <Signin />
                 
