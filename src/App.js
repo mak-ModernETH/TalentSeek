7809d7d798b07201e099cd230907640c5be3892c
ms34 <onethreems@gmail.com>
2019-07-24 15:05:54 +0300

    update app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, {Component} from 'react';
-import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
-import Logo from './components/Logo/Logo';
-import Landing from './components/Landing/Landing';
+import Particles from 'react-particles-js';
+import Home from './components/Home/Home'
+ import Logo from './components/Logo/Logo';
 
-const particlesOptions = {
-        particles: {
-            line_linked: {
-                shadow: {
-                    enable: true,
-                    color: "#3CA9D1",
-                    blur: 5
+const particleOptions ={
+                    particles: {
+                     number:{
+
+                        value:80,
+                        density:{
+                            enable:true,
+                            value_area:800
+                        }
+                     }
+                    }
                 }
-            }
-        }
-}
 class App extends Component {
+
+
     constructor() {
         super()
         this.state = {
@@ -28,12 +31,13 @@ class App extends Component {
     render() {
         return(
             <div className="App">
-                <Particles
-                    params={particlesOptions}
-                 />
+
+            <Particles className="particles" 
+              params={particleOptions}
+             
+            />
                 <Navigation />
                 <Logo />
-                {/* <Landing /> */}
                 <Signin />
                 
             </div>
