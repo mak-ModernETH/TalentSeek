0a6d106a2f761b9f33a812fce97dda893d70abd6
ms34 <onethreems@gmail.com>
2019-07-24 12:41:28 +0300

    update app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
 import Logo from './components/Logo/Logo';
 import Landing from './components/Landing/Landing';
 
+// const particlesOptions = {
+//         particles: {
+//             line_linked: {
+//                 shadow: {
+//                     enable: true,
+//                     color: "#3CA9D1",
+//                     blur: 5
+//                 }
+//             }
+//         }
+// }
 class App extends Component {
     constructor() {
         super()
@@ -16,6 +28,9 @@ class App extends Component {
     render() {
         return(
             <div className="App">
+                {/* <Particles
+                    params={particlesOptions}
+                 /> */}
                 <Navigation />
                 <Logo />
                 {/* <Landing /> */}
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import './Logo.css'
 const Logo = () => {
     return (
         <div className="ma4 mt0">
-            <Tilt className="Tilt br2 shadow-2" options={{ max: 25 }} >
+            <Tilt className="Tilt br2 shadow-5" options={{ max: 25 }} >
                 <div className="Tilt-inner"><img src={talent} alt="logo"/></div>
             </Tilt>
         </div>
