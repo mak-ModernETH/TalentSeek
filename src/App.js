6384255ed62e2bd2c170e7e19baa9430109195b4
asfawayalk <asfaw.ayalkibet@gmail.com>
2019-07-24 12:52:05 +0300

    insering app.js

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Particles from 'react-particles-js';
+import Home from './components/Home/Home'
+ import Logo from './components/Logo/Logo';
+
+const particleOptions ={
+                    particles: {
+                     number:{
+
+                        value:80,
+                        density:{
+                            enable:true,
+                            value_area:800
+                        }
+                     }
+                    }
+                }
+class App extends Component {
+
+
+    constructor() {
+        super()
+        this.state = {
+            input: '',
+            route: 'signin'
+        }
+    }
+    render() {
+        return(
+            <div className="App">
+
+            <Particles className="particles" 
+              params={particleOptions}
+             
+            />
+                <Navigation />
+                <Logo />
+                <Signin />
+                
+            </div>
+        )
+    }
+}
+export default App;
