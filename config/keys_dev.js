ed376f26fa9946ca5c11bba6d61cb44a3f17f0ed
ms34 <onethreems@gmail.com>
2019-07-29 13:04:26 +0300

    addint entry point in json.package and update login and sign up pages

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
     // mongoURI: 'mongodb+srv://dev:dev@cluster0-oscfe.mongodb.net/test?retryWrites=true&w=majority',
-    mongoURI: 'mongodb+srv://dev:dev@cluster0-oscfe.mongodb.net/test?retryWrites=true&w=majority',
+    mongoURI: 'mongodb+srv://mak:mak@cluster0-oscfe.mongodb.net/test?retryWrites=true&w=majority',
     secretOrKey: 'SECRET'
 };
