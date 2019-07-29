2b2bb8c9e01c3a6f44b9991ada8d0870233cb0f8
ms34 <onethreems@gmail.com>
2019-07-29 16:14:11 +0300

    User and profile files fixed

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,6 @@
 module.exports = {
     // mongoURI: 'mongodb+srv://dev:dev@cluster0-oscfe.mongodb.net/test?retryWrites=true&w=majority',
-    mongoURI: 'mongodb+srv://mak:mak@cluster0-oscfe.mongodb.net/test?retryWrites=true&w=majority',
+    // mongoURI: 'mongodb+srv://mak:mak@cluster0-oscfe.mongodb.net/test?retryWrites=true&w=majority',
+    mongoURI: "mongodb://localhost:27017/TalentSeek",
     secretOrKey: 'SECRET'
 };
